57ce884ea63827be9b969195ce05281391e42b09
codexdevelop <tryoutbothai@gmail.com>
2025-03-09 23:23:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1476,18 +1476,29 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
     temp.FILES_ID[f"{message.chat.id}-{message.id}"] = batch_ids
     batch_link = f"batchfiles#{message.chat.id}#{message.id}#{message.from_user.id}"
     temp.CHAT[message.from_user.id] = message.chat.id
-    settings = await get_settings(message.chat.id , pm_mode=pm_mode)
-    del_msg = f"\n\n<b>🪃 ᴛʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴀꜰᴛᴇʀ <code>{get_readable_time(DELETE_TIME)}</code> ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ ɪssᴜᴇs</b>" if settings["auto_delete"] else ''
+    settings = await get_settings(query.message.chat.id)
+    reqnxt  = query.from_user.id if query.from_user else 0
+    temp.CHAT[query.from_user.id] = query.message.chat.id
+
     links = ""
     if settings["link"]:
         btn = []
-        for file_num, file in enumerate(files, start=1):
-            links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start={"pm_mode_" if pm_mode else ''}file_{ADMINS[0] if pm_mode else message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {formate_file_name(file.file_name)}</a></b>"""
-    else:    
-        BLOGSPOT_URL = "https://technoji786.blogspot.com/2025/02/codex-channel.html"
-        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)} ≽ {formate_file_name(file.file_name)}",url=f"{BLOGSPOT_URL}?file={file.file_id}")] 
-	       for file in files
-	      ]
+        for file_num, file in enumerate(files, start=offset+1):links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}</a></b>"""
+    else: 
+        # Shortener apply karein based on verification level
+        verification_level = settings.get("verification_level", 1)
+
+        short_url = f"{SHORTENER_WEBSITE}/shorten?url={file.file_id}"
+        if verification_level >= 2:
+        short_url = f"{SHORTENER_WEBSITE2}/shorten?url={short_url}"
+        if verification_level == 3:
+        short_url = f"{SHORTENER_WEBSITE3}/shorten?url={short_url}"
+
+        # Blogspot Redirection Setup
+        BLOGSPOT_URL = "https://technoji786.blogspot.com/redirect.html"
+        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)} ≽ {formate_file_name(file.file_name)}",url=f"{BLOGSPOT_URL}?file={short_url}")]
+        for file in files
+          ]
     if offset != "":
         if total_results >= MAX_BTN:
             btn.insert(0,[
